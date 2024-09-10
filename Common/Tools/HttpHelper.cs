0adb4d2cbe3cfc889e72f9f4fd24751c34d29b1b
Juan Trujillo <jd.trujillov@gmail.com>
2024-09-10 18:52:38 -0500

    Cambios

---
+++ Common/Tools/HttpHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.Tools;
+
+public class HttpHelper
+{
+    public HttpHelper(string baseUrl, string token = "") { _baseUrl = baseUrl; _token = token; }
+    private readonly string _baseUrl;
+    private readonly string _token;
+
+}
