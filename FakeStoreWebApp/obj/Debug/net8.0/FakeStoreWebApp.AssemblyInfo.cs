5583feee8add5967e998254b0c0646608419f81f
DylanDevRamirezz <159856318+DylanDevRamirezz@users.noreply.github.com>
2024-09-10 18:57:21 -0500

    Mas cambios

--- FakeStoreWebApp/obj/Debug/net8.0/FakeStoreWebApp.AssemblyInfo.cs
+++ FakeStoreWebApp/obj/Debug/net8.0/FakeStoreWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FakeStoreWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0adb4d2cbe3cfc889e72f9f4fd24751c34d29b1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a48413015de245864d823b964505b172e46b713")]
 [assembly: System.Reflection.AssemblyProductAttribute("FakeStoreWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FakeStoreWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
