c28cfc27f194d6f6f0fe18a703261d18913bf579
Juan Trujillo <jd.trujillov@gmail.com>
2024-09-10 20:20:16 -0500

    cambios

--- Common/Contracts/ICart.cs
+++ Common/Contracts/ICart.cs
@@ -3,7 +3,8 @@
 public interface ICart
 {
     Task<ICollection<Cart>> GetAllAsync();
-    Task<Cart> GetAsync(int userid);
+    Task<Cart> GetAsync(int cartid);
+    Task<Cart> GetUserAsync(int userid);
     Task<Cart> InsertAsync(Cart carrito);
     Task<Cart> UpdateAsync(int id, Cart carrito);
     Task<Cart> PatchAsync(int id, Cart carrito);
--- Common/Rest/RestCart.cs
+++ Common/Rest/RestCart.cs
@@ -5,31 +5,24 @@ public class RestCart : ICart
     public RestCart(string baseUrl, string token) { http = new HttpHelper(baseUrl, token); }
     private readonly HttpHelper http;
 
+    public async Task<ICollection<Cart>> GetAllAsync()
+        => await http.GetAsync<ICollection<Cart>>("carts");
+
+    public async Task<Cart> GetAsync(int cartid)
+        => await http.GetAsync<Cart>($"carts/{cartid}");
+
+    public async Task<Cart> GetUserAsync(int userid)
+        => await http.GetAsync<Cart>($"carts/user/{userid}");
+
+    public async Task<Cart> InsertAsync(Cart carrito)
+        => await http.PostAsync<Cart, Cart>("carts", carrito);
+
+    public async Task<Cart> UpdateAsync(int id, Cart carrito)
+        => await http.PutAsync<Cart, Cart>($"carts/{id}", carrito);
+
+    public async Task<Cart> PatchAsync(int id, Cart carrito)
+        => await http.PatchAsync<Cart, Cart>($"carts/{id}", carrito);
+
     public async Task<bool> DeleteAsync(int id)
         => await http.DeleteAsync($"carts/{id}");
-
-    public Task<ICollection<Cart>> GetAllAsync()
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<Cart> GetAsync(int userid)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<Cart> InsertAsync(Cart carrito)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<Cart> PatchAsync(int id, Cart carrito)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<Cart> UpdateAsync(int id, Cart carrito)
-    {
-        throw new NotImplementedException();
-    }
 }
