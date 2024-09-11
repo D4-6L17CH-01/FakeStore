f725595f6e40a1ceecabf0c5a6f15e2378db0856
DylanDevRamirezz <159856318+DylanDevRamirezz@users.noreply.github.com>
2024-09-10 19:43:30 -0500

    Contracts y GlobalUsings

---
+++ Common/Contracts/IProducts.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.Contracts;
+
+public interface IProducts
+{
+    Task<ICollection<Product>> GetAllAsync();
+    Task<Product> GetAsync();
+    Task<Product> InsertAsync(Product producto);
+    Task<Product> UpdateAsync(int id , Product producto);
+    Task<bool> DeleteAsync(int id);
+}
