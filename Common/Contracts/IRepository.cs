16adaf804339b47d623a02d0447a884efe8c6b45
DylanDevRamirezz <159856318+DylanDevRamirezz@users.noreply.github.com>
2024-09-10 20:22:22 -0500

    Repoository

---
+++ Common/Contracts/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Contracts;
+
+public interface IRepository
+{
+    void SetToken(string token);
+    void SetUrl(string url);
+    IProducts Products { get; }
+    ICart Cart { get; }
+    IUser User { get; }
+    ILogin Login { get; }
+}
---
+++ Common/Rest/RestRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Rest;
+
+public class RestRepository : IRepository
+{
+    private string token;
+    public void SetToken(string token)
+        => this.token = token;  
+    private string url;
+    public void SetUrl(string url)
+        => this.url = url;
+    public RestRepository(string url_)
+        => this.url = url_;
+    public IProducts Products => new RestProduct(url, token);
+
+    public ICart Cart => new RestCart(url, token);
+
+    public IUser User => new RestUser(url, token);
+
+    public ILogin Login => new RestLogin(url, token);
+}
