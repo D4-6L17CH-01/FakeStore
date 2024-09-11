89ad10c0f9bebbb488c7c267cf3f9e84df334ba9
DylanDevRamirezz <159856318+DylanDevRamirezz@users.noreply.github.com>
2024-09-10 19:54:00 -0500

    cambios

---
+++ Common/Rest/RestProduct.cs
@@ -0,0 +1,38 @@
+﻿using Common.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Rest;
+
+public class RestProduct : IProducts
+{
+    private readonly HttpHelper;
+    public RestProduct()
+    {
+                
+    }
+    public Task<ICollection<Product>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> GetAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> InsertAsync(Product producto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> UpdateAsync(int id, Product producto)
+    {
+        throw new NotImplementedException();
+    }
+    public Task<bool> DeleteAsync(int id)
+        => 
+}
