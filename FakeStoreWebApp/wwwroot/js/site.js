9d8d5f208ab65ec63e7c1415825f1970959ec4c1
Juan Trujillo <jd.trujillov@gmail.com>
2024-09-11 19:02:09 -0500

    Can

---
+++ FakeStoreWebApp/wwwroot/js/site.js
@@ -0,0 +1,56 @@
+﻿window.AgregarClaseElemento = (Id, Classes) => {
+    const element = document.getElementById(Id);
+    if (element) {
+        Classes.forEach(className => {
+            element.classList.add(className);
+        })
+    }
+}
+
+window.InicializarTooltips = () => {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+}
+
+window.BoolNotification = (titulo, mensaje, icono) => {
+    return new Promise(resolve => {
+        Swal.fire({
+            title: titulo,
+            text: mensaje,
+            icon: icono,
+            showDenyButton: true,
+            showCancelButton: false,
+            confirmButtonText: "Si",
+            denyButtonText: "No"
+        }).then((result) => {
+            resolve(result.isConfirmed);
+        });
+    })
+}
+
+window.printInvoice = (elementId) => {
+    var printWindow = window.open('', '', 'height=500,width=800');
+    printWindow.document.write('<html><head><title>Imprimir Factura</title>');
+    printWindow.document.write('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">');
+    printWindow.document.write('<link rel = "stylesheet" href = "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">'); 
+    printWindow.document.write('<link rel="stylesheet" href="css/app.css" />');
+    printWindow.document.write('</head><body class="container"><div class="container">');
+    printWindow.document.write(document.getElementById(elementId).innerHTML);
+    printWindow.document.write('</div></body></html>');
+    printWindow.document.close();
+    printWindow.print();
+    printWindow.onafterprint = function () {
+        printWindow.close();
+    };
+}
+
+window.gotoURl = (url) => {
+    window.location.replace(url);
+}
+
+//Implementar DataTable
+window.initializeDataTable = function initializeDataTable(table) {
+    $(document).ready(function () {
+        $(table).DataTable();
+    });
+}
--- Model/obj/Debug/net8.0/Model.AssemblyInfo.cs
+++ Model/obj/Debug/net8.0/Model.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3411b7ddabfb93ae7b8f3f09ad6d50615418b9c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb1c1129c709bd6992e79550e4cfac6e3f7352b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
