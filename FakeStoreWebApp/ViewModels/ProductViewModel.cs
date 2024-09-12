7f8c322c67105a2f58095881eda66323c2ba0b73
Juan Trujillo <juan.trujillo@syscafe.com>
2024-09-11 22:59:04 -0500

    ca

--- FakeStoreWebApp/ViewModels/ProductViewModel.cs
+++ FakeStoreWebApp/ViewModels/ProductViewModel.cs
@@ -5,8 +5,14 @@ public partial class ProductViewModel : BaseViewModel
 {
     public ProductViewModel(IServiceProvider serviceProvider) : base(serviceProvider)
     {
+        repository = serviceProvider?.GetService<IRepository>();
+        notificationService = serviceProvider?.GetService<INotificationService>()!;
+        navigationManager = serviceProvider?.GetService<NavigationManager>();
     }
-
+    private readonly IRepository? repository;
+    private readonly INotificationService notificationService;
+    private readonly NavigationManager? navigationManager;
+    
     [ObservableProperty]
     private Product product;
 }
