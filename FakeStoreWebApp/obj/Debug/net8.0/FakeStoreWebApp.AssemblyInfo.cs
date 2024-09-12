7d330c766863e3ddce366b1a9256c6f9779e2157
DylanDevRamirezz <159856318+DylanDevRamirezz@users.noreply.github.com>
2024-09-11 21:31:10 -0500

    Prueba de Grid con Productos

--- FakeStoreWebApp/obj/Debug/net8.0/FakeStoreWebApp.AssemblyInfo.cs
+++ FakeStoreWebApp/obj/Debug/net8.0/FakeStoreWebApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FakeStoreWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+597b29a5ea620e962e3146b11e354e701c455678")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0d87c1eede773faec9c684421b5f2e7424d3832")]
 [assembly: System.Reflection.AssemblyProductAttribute("FakeStoreWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FakeStoreWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Model/obj/Debug/net8.0/Model.AssemblyInfo.cs
+++ Model/obj/Debug/net8.0/Model.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb1c1129c709bd6992e79550e4cfac6e3f7352b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0d87c1eede773faec9c684421b5f2e7424d3832")]
 [assembly: System.Reflection.AssemblyProductAttribute("Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
